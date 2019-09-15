9a2aab7d8fd2e6fa11a9f94b980f0ed183859664
dekaqqq <kdrushlyak@gmail.com>
2019-09-15 10:24:47 +0300

    Updates

---
+++ precache-manifest.ad8eec9caf892af3da457d99e15d75f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3668a21efb161a6d0a020c87761959",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "2c061812b3636ef1c4f5",
+    "url": "/goit-react-hw-01-components/static/css/main.43c38f55.chunk.css"
+  },
+  {
+    "revision": "bc5909283cdaccad1c4c",
+    "url": "/goit-react-hw-01-components/static/js/2.50cc313e.chunk.js"
+  },
+  {
+    "revision": "2c061812b3636ef1c4f5",
+    "url": "/goit-react-hw-01-components/static/js/main.6139bb65.chunk.js"
+  },
+  {
+    "revision": "620ceb3fc25e2e9c8f81",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.a6ab4004.js"
+  },
+  {
+    "revision": "a3fd13edf050b8cf12647235ccf29c1e",
+    "url": "/goit-react-hw-01-components/static/media/avatar.a3fd13ed.png"
+  }
+]);
